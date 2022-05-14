5ae01277f48aa75c9c4ff20eb7800110ed231060
bolero2 <sheocjf1025@gmail.com>
2022-05-14 10:19:53 +0900

    cicd

--- cicd/add_prefix.py
+++ cicd/add_prefix.py
@@ -5,17 +5,9 @@ prefix = os.getenv("BOLERO2_GITHUB_IO_INDEX_HTML_PREFIX")
 repo = os.getenv("BOLERO2_REPOSITORY")
 
 
-print(prefix)
-print(repo)
+print("PREFIX :", prefix)
+print("REPOSITORY :", repo)
 
-
-string = """
----
-title: bolero2.log
-subtitle: AI Engineer's Archives
-layout: page
-callouts: home_callouts
-show_sidebar: true
----
-"""
-print(string)
+with ("~/prefix.md", 'w') as f:
+    f.write(prefix)
+:
